c26e541eb78263bde4a3f0161270443588645fb2
friendly-flame <armsnyder@gmail.com>
2015-09-14 07:51:03 -0500

    test distance

--- tests/tests.py
+++ tests/tests.py
@@ -14,3 +14,12 @@ class Test(unittest.TestCase):
                 found_non_zero = True
                 break
         self.assertTrue(found_non_zero)
+
+    def test_distance(self):
+        in_a = [1, 2, 3, 4, 5]
+        in_b = [-2, 7, 11, -9]
+        expected = 3+5+8+13
+        actual_a = door_buzz_enhancer.get_sound_distance(in_a, in_b)
+        actual_b = door_buzz_enhancer.get_sound_distance(in_b, in_a)
+        self.assertEqual(actual_a, actual_b)
+        self.assertEqual(expected, actual_b)
